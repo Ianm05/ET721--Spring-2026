e181f5122d313986054c577dd111161e664fdf93
Ian Martinez <Imartinez@qcc.cuny.edu>
2026-01-29 22:19:38 +0000

    update testing

--- Lab2_lastname.py
+++ Lab2_lastname.py
@@ -14,4 +14,8 @@ print(f'\nWelcome to Git commands{username}')
 
 # testing lines 
 num = 8
+<<<<<<< HEAD
 print(f'Double of number {num} is {num * 2}')
+=======
+print(f'Double of number {num} is {num * 2}')
+>>>>>>> e98c241 (update testing)
