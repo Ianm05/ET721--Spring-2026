9ee042d1fc1cfa257668f0328cc8d9d70ae43920
Ian Martinez <Imartinez@qcc.cuny.edu>
2026-01-29 22:15:02 +0000

    update testing

--- Lab2_lastname.py
+++ Lab2_lastname.py
@@ -5,8 +5,13 @@ lab 2: GIT commands
 username = input("Enter a username:")
 print(f'\nWelcome to Git commands{username}')
 
-# testing 2 calculate the hypotenuse 
-X = 5
-y=3
-hyp = (X**2 + y**2)**0.5
-print(f'The hypotenuse of {X} and {y} is {hyp}0.2:f')
+"""
+Ian Martinez
+lab 2: GIT commands
+"""
+username = input("Enter a username:")
+print(f'\nWelcome to Git commands{username}')
+
+# testing lines 
+num = 8
+print(f'Double of number {num} is {num * 2}')
