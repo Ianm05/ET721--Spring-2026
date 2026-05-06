4c7892d99b23607356832ff95a6ccd3a408a8e96
Ianm05 <ianm59034@gmail.com>
2026-05-06 23:02:41 +0000

    Lab19

---
+++ lab19/app.py
@@ -0,0 +1,85 @@
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for, session, flash 
+
+app = Flask(__name__)
+app.secret_key = "dev_secret_key" 
+
+#----------------------
+# DATABASE CONNECTION
+#----------------------
+def get_db():
+    conn = sqlite3.connect("flask_auth.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+#----------------------
+# LOADING PAGE
+#----------------------
+@app.route('/')
+def home():
+    return redirect(url_for('login'))  
+
+#--------------------------
+# LOGIN ROUTING
+#--------------------------
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        conn = get_db()
+        cursor = conn.cursor()
+        
+        cursor.execute("""SELECT * FROM users WHERE email = ? AND password = ?""", (email, password))
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            session['username'] = user['username']
+            return redirect(url_for('dashboard')) 
+        else:
+            flash("Invalid email or password")
+
+    return render_template('login.html')
+
+#----------------------
+# SIGNUP ROUTING
+#----------------------
+@app.route('/signup', methods=['GET', 'POST'])  
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        conn = get_db()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute("""INSERT INTO users (username, email, password) VALUES (?, ?, ?)""", (username, email, password))  # Fix 3: fixed indentation
+            conn.commit()  # Fix 4: moved inside try block
+            flash("Account created successfully!.")
+            return redirect(url_for('login'))  
+        except sqlite3.IntegrityError:
+            flash("Email already exists.")
+        finally:
+            conn.close()
+
+    return render_template('signup.html') 
+
+#--------------------------
+# DASHBOARD ROUTING
+#--------------------------
+@app.route('/dashboard')  
+def dashboard():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    
+    return render_template('dashboard.html')
+
+#--------------------------
+# RUN APP
+#--------------------------
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ lab19/test_app.py
@@ -0,0 +1,103 @@
+import os
+import sqlite3
+import pytest
+from app import app
+#---------------------------------
+# TEST DATABASE SETUP
+#---------------------------------
+TEST_DB = "test_flask_auth.db"
+
+def init_test_db():
+    #simulate a database connection
+    conn = sqlite3.connect(TEST_DB)
+    cursor = conn.cursor()
+
+    # create a temple table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL
+        )
+        
+    """)
+    conn.commit()
+    conn.close()
+
+# create a mock database to run the app.py file\
+@pytest.fixture
+def client(monkeypatch):
+    # override database to use test database
+    def get_test_db():
+        conn = sqlite3.connect(TEST_DB)
+        conn.row_factory = sqlite3.Row
+        return conn
+
+    # match the mock database
+    from app import get_db
+    monkeypatch.setattr('app.get_db', get_test_db)
+    app.config['TESTING'] = True
+    app.config['SECRET_KEY'] = 'test_secret'
+
+    # call function to run the mock database
+    init_test_db()
+
+    with app.test_client() as client:
+        yield client
+
+    # cleanup test database after tests
+    if os.path.exists(TEST_DB):
+        os.remove(TEST_DB)
+#---------------------------------
+# TEST HOME REDIRENT
+#---------------------------------
+def test_home_redirect(client):
+    response = client.get('/')
+    assert response.status_code == 302
+    assert 'login' in response.location # response.location means properly redirect to the URL location
+
+#---------------------------------
+# TEST LOGIN SUCCESS
+#---------------------------------
+def test_login_success(client):
+   # first, create a test user to test the login later
+   client.post('/signup', data={
+         'username': "loginuser",
+         'email': "loginuser@example.com",
+         'password': "123456"
+   })
+
+   # test the login with the user info above
+   response = client.post('/login', data={
+        "email": "loginuser@example.com",
+        "password": "123456"
+   }, follow_redirects=True)
+
+   # assert testing
+   assert response.status_code == 200
+   assert b"Welcome" in response.data
+#---------------------------------
+# TEST LOGIN FAILURE
+#---------------------------------
+def test_login_failure(client):
+    response  = client.post('/login' , data = {
+        "email" : "login@example.com",
+        "password" : "wrong123"
+    }, follow_redirects = True)
+    
+    assert response.status_code == 200
+    assert b"Invalid email or password" in response.data
+
+#---------------------------------
+# TEST SIGNUP
+#---------------------------------
+def test_signup(client):
+    response = client.post('/signup', data ={
+        "username" : "testuser",
+        "email" : "test@example.com",
+        "password" : "123456"
+     }, follow_redirects = True)
+
+    assert response.status_code == 200
+    assert b"Account created successfully!" in response.data
